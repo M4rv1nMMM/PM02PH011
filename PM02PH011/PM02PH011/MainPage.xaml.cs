9b37df55bce82cb56e04a0fed8647c68869642e6
M4rv1nMMM <marvinmiguel3@gmail.com>
2021-09-26 18:25:37 -0600

    Add project files.

---
+++ PM02PH011/PM02PH011/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PM02PH011
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAgregar_Clicked(object sender, EventArgs e)
+        {
+            var num1 = Convert.ToDecimal(txtnum1.Text);
+            var num2 = Convert.ToDecimal(txtnum2.Text);
+
+            await Navigation.PushAsync(new SecondPage(Convert.ToDecimal(num1 + num2)));
+
+            //wait DisplayAlert("Suma", Convert.ToString(num1 + num2), "OK");
+        }
+    }
+}
---
+++ PM02PH011/PM02PH011/Models/Operaciones.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM02PH011.Models
+{
+    class Operaciones
+    {
+    }
+}
